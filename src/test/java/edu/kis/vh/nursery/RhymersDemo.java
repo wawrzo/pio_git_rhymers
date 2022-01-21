b83a6a2c5244e5dba3edd9fa732b5a1c52b18934
wawrzo <wawrzonowskipiotr@gmail.com>
2022-01-21 13:32:18 +0100

    1.5.6 documentation for RhymersDemo class and its methods added

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -3,6 +3,12 @@ package edu.kis.vh.nursery;
 import edu.kis.vh.nursery.factory.DefaultRhymersFactory;
 import edu.kis.vh.nursery.factory.Rhymersfactory;
 
+/**
+ * RhymesDemo class has two methods: main and testRhymers;
+ * main method is used for making new class and run second method
+ * testRhymers method is used for making list of all of the classes and testing them
+ */
+
 class RhymersDemo {
 
 	public static void main(String[] args) {
